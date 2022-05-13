ea990b7a5bf5303db380970bbc7d2504abb07024
fatemeh ameri <fatemehamerizead@gmail.com>
2022-05-13 19:04:50 +0430

    Questions added

--- Hello_World/hello_world.py
+++ Hello_World/hello_world.py
@@ -1,2 +1,6 @@
+# Modify the provided code so that it produces the string "Hello, World!".
+# Run the test suite and make sure that it succeeds.
+# Submit your solution and check it at the website.
+
 def hello():
     return 'Hello, World!'
--- Two_Fer/two_fer.py
+++ Two_Fer/two_fer.py
@@ -1,2 +1,8 @@
+# Given a name, return a string with the message:
+# One for name, one for me.
+# Where "name" is the given name.
+# However, if the name is missing, return the string:
+# One for you, one for me.
+
 def two_fer (name = 'you'): 
     return 'One for {0}, one for me.'.format(name)
