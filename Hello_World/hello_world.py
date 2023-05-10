64a25e77a716ce385941957dcd710dd4b24e1bee
Fatemeh Ameri <35129870+Fatemeh-ameri@users.noreply.github.com>
2023-05-10 13:39:31 +0430

    Some things were removed!

--- Hello_World/hello_world.py
+++ Hello_World/hello_world.py
@@ -1,6 +1,2 @@
-# Modify the provided code so that it produces the string "Hello, World!".
-# Run the test suite and make sure that it succeeds.
-# Submit your solution and check it at the website.
-
 def hello():
     return 'Hello, World!'
