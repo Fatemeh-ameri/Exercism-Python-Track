1d23db419d1da3067193de54bb17707cb6ff4ff1
Fatemeh Ameri <35129870+Fatemeh-ameri@users.noreply.github.com>
2023-11-01 21:28:55 +0330

    Two_Fer remove

--- Two_Fer/two_fer.py
+++
@@ -1,8 +0,0 @@
-# Given a name, return a string with the message:
-# One for name, one for me.
-# Where "name" is the given name.
-# However, if the name is missing, return the string:
-# One for you, one for me.
-
-def two_fer (name = 'you'): 
-    return 'One for {0}, one for me.'.format(name)
